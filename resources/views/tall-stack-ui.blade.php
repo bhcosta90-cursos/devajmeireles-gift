344453a686c12bfb718a861effe6ae6f847e4591
Bruno Costa <bhcosta90@gmail.com>
2024-08-01 20:48:54 -0300

    wip

--- resources/views/tall-stack-ui.blade.php
+++ resources/views/tall-stack-ui.blade.php
@@ -1,3 +1,15 @@
-<x-layout.app>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ $title ?? 'Page Title' }}</title>
+
+    <tallstackui:script />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
     <x-ts-tag />
-</x-layout.app>
+</body>
+</html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    presets: [
+        require('./vendor/tallstackui/tallstackui/tailwind.config.js')
+    ],
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        './vendor/tallstackui/tallstackui/src/**/*.php',
     ],
   theme: {
     extend: {},
