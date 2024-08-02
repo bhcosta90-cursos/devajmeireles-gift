a7f3f35838059b7088a300a720cfa21b83ca9ff2
Bruno Costa <bhcosta90@gmail.com>
2024-08-02 14:06:51 -0300

    wip

--- app/Models/Trait/Search.php
+++ app/Models/Trait/Search.php
@@ -1,23 +1,25 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace App\Models\Trait;
 
 trait Search
 {
-    public function scopeSearch($query, array $search): void{
+    public function scopeSearch($query, array $search): void
+    {
         $keys = array_keys($search);
 
         $newValues = [];
-        foreach($search as $value) {
-            foreach($value as $v) {
+
+        foreach ($search as $value) {
+            foreach ($value as $v) {
                 $newValues[] = $v;
             }
         }
 
-        if($newValues) {
-            $query->where(function($query) use($keys, $newValues){
+        if ($newValues) {
+            $query->where(function ($query) use ($keys, $newValues) {
                 foreach ($newValues as $value) {
                     $query->orWhereAny($keys, 'like', "%{$value}%");
                 }
--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -1,11 +1,11 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
-use Laravel\Telescope\IncomingEntry;
-use Laravel\Telescope\Telescope;
-use Laravel\Telescope\TelescopeApplicationServiceProvider;
+use Laravel\Telescope\{IncomingEntry, Telescope, TelescopeApplicationServiceProvider};
 
 class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
 {
