775b0027c0da93aa40803e04da16388a012ef489
Bruno Costa <bhcosta90@gmail.com>
2024-08-03 14:36:32 -0300

    wip

--- app/Livewire/Admin/Items.php
+++ app/Livewire/Admin/Items.php
@@ -4,8 +4,8 @@ declare(strict_types = 1);
 
 namespace App\Livewire\Admin;
 
-use App\Livewire\Admin\Traits\{Table};
 use App\Livewire\Admin\Traits\Dialog;
+use App\Livewire\Admin\Traits\{Table};
 use App\Models\Item;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Contracts\View\View;
