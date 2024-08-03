06575c7e02b2cf7867ba7eed82e5cb0dac0368ed
Bruno Costa <bhcosta90@gmail.com>
2024-08-03 15:28:29 -0300

    wip

--- resources/views/components/ui/table/index.blade.php
+++ resources/views/components/ui/table/index.blade.php
@@ -5,6 +5,16 @@
 <div class="overflow-auto rounded-lg shadow ring-1 ring-black ring-opacity-5 custom-scrollbar">
     <table class="min-w-full divide-y divide-gray-300">
         {{ $slot }}
+
+        @if($records->count() === 0)
+            <tfoot>
+                <x-ui.table.tr>
+                    <x-ui.table.td colspan="30" class="text-center text-xl">
+                        @lang('No results found.')
+                    </x-ui.table.td>
+                </x-ui.table.tr>
+            </tfoot>
+        @endif
     </table>
     @if($records instanceof Paginator && $records->hasMorePages())
         <div class="p-3 bg-gray-50 border-t border-t-gray-300">
