6d048430ee32a26472d9d7b2f32acc4b3c474caf
Bruno Costa <bhcosta90@gmail.com>
2024-08-04 12:16:33 -0300

    wip

--- routes/api.php
+++ routes/api.php
@@ -4,50 +4,16 @@ declare(strict_types = 1);
 
 use App\Action\Filter\SearchFilter;
 use App\Models\{Category, Item};
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::name('api.')->group(function () {
-    Route::get('/categories', fn (Category $category, Request $request) => $category
-        ->active()
-        ->orderBy('name')
-        ->limit(30)
-        ->search([
-            'name' => [$request->get('search')],
-        ])
-        ->when($selected = $request->get('selected'), fn ($query) => $query->whereIn('id', json_decode($selected)))
-        ->get()
-        ->map(function (Category $category) {
-            return [
-                'id'   => $category->id,
-                'name' => $category->name,
-            ];
-        })
-        ->toArray())->name('categories');
+    Route::get(
+        '/categories',
+        fn (Category $category, SearchFilter $searchFilter) => $searchFilter->handle($category->active()->orderBy('name'))
+    )->name('items');
 
     Route::get(
         '/items',
-        fn (Item $item, SearchFilter $searchFilter) => $searchFilter->handle(
-            $item
-                ->active()
-                ->orderBy('name')
-        )
+        fn (Item $item, SearchFilter $searchFilter) => $searchFilter->handle($item->active()->orderBy('name'))
     )->name('items');
-
-    Route::get('/v1/items', fn (Item $item, Request $request) => $item
-        ->active()
-        ->orderBy('name')
-        ->search([
-            'name' => [$request->get('search')],
-        ])
-        ->when($selected = $request->get('selected'), fn ($query) => $query->whereIn('id', json_decode($selected)))
-        ->limit(30)
-        ->get()
-        ->map(function (Item $item) {
-            return [
-                'id'   => $item->id,
-                'name' => $item->name,
-            ];
-        })
-        ->toArray())->name('items');
 });
