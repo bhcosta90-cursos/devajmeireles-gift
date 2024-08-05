8ccede2e919fcc6ef4a914447aaf90c631244537
Bruno Costa <bhcosta90@gmail.com>
2024-08-05 18:29:31 -0300

    wip

--- resources/views/livewire/dashboard/card.blade.php
+++ resources/views/livewire/dashboard/card.blade.php
@@ -2,20 +2,16 @@
     <x-ui.card>
         <div class="ml-4 p-2">
             <div class="flex items-center justify-start">
-                <div class="flex-shrink-0">
-                    <p class="text-3xl font-semibold text-primary">
+                <div class="flex-shrink-0 rounded p-2 bg-primary-100">
+                    <p class="text-3xl font-bold text-primary-600">
                         <span wire:loading>...</span>
                         <span wire:loading.remove>{{ $this->quantity }}</span>
                     </p>
                 </div>
                 <div class="ml-4">
-                    <h3 class="text-base font-semibold leading-6 text-gray-900" title="TEST">
+                    <h3 class="font-semibold leading-6 text-md text-primary">
                         {{ $this->card->label() }}
                     </h3>
-                    <x-ui.badge outline primary>
-                        <span wire:loading>...</span>
-                        <span wire:loading.remove>80% em fraldas</span>
-                    </x-ui.badge>
                 </div>
             </div>
         </div>
