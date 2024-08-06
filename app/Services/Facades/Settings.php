f5ee20503b689e4c7c1b71bf7bd79a03bb9a0ada
Bruno Costa <bhcosta90@gmail.com>
2024-08-06 17:30:18 -0300

    wip

--- app/Services/Facades/Settings.php
+++ app/Services/Facades/Settings.php
@@ -4,8 +4,14 @@ declare(strict_types = 1);
 
 namespace App\Services\Facades;
 
+use App\Models\Setting;
 use App\Services\SettingService;
 
+/**
+ * @method static string|null get(string $key, mixed $default = null)
+ * @method static Setting set(string $key, mixed $value, string $type = 'string')
+ * @see SettingsService
+ */
 class Settings
 {
     protected static function getFacadeAccessor(): string
