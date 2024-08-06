2b1a794f544baea49b8bbf42f46c9211a89fc5a9
Bruno Costa <bhcosta90@gmail.com>
2024-08-06 16:06:18 -0300

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 
 namespace App\Providers;
 
+use App\Services\Facades\Settings;
+use App\Services\SettingService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,6 +19,12 @@ class AppServiceProvider extends ServiceProvider
             $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
             $this->app->register(TelescopeServiceProvider::class);
         }
+
+        $this->app->bind(
+            Settings::class,
+            fn () => $this->app->make(SettingService::class)
+        );
+
     }
 
     /**
